58771c9382c594e9b4e9c989807fef156bd3c96a
celine-adamo <71389471+celine-adamo@users.noreply.github.com>
2021-01-21 11:06:51 +0100

    thumbnails

---
+++ assets/thumbnails.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+    <link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/inscription.css">
+
+</head>
+
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+            <a class="navbar-brand"><img src="../assets/img/inscription.png" alt="abyss2" class="abyss-thum"></a>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+    </section>
+
+</body>
